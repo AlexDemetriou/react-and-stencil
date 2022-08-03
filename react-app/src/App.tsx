b98921ae5dc028b0cfafa1d087a6b37b026559ab
Alex Demetriou <alex.andreas.demetriou@outlook.com>
2022-08-03 08:40:31 +0100

    first commit?

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import { MyComponent } from 'react-component-library';
+
+function App() {
+  return (
+    <MyComponent first='Alex' middle='Andreas' last='Demetriou'/>
+  );
+}
+
+export default App;
